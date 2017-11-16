7552e63d2668b9bcbad15138c77e034013dc83b5
Marko Koron <koron.marko@gmail.com>
2017-11-16 21:53:46 +0100

    Written all code, doesn't work 100%

--- battleship.py
+++ battleship.py
@@ -1,10 +1,12 @@
 import random
 
-#Create board and setup game
+# Create board and setup game
+
 
 class Direction:
     HORIZONTAL = 0
     VERTICAL = 1
+
 
 class Field:
     def __init__(self, row, column):
@@ -13,16 +15,18 @@ class Field:
         self._free = True
 
     def __eq__(self, otherField):
-        if type(otherField) is not type(self):
+        if not isinstance(otherField, type(self)):
             return False
- 
-        return ((self._row == otherField._row) and (self._column == otherField._column))
+
+        return ((self._row == otherField._row) and (
+            self._column == otherField._column))
 
     def __str__(self):
         return "Field({}, {})".format(self._row, self._column)
 
     def __hash__(self):
         return hash((self._row, self._column))
+
 
 class Board:
     def __init__(self, rowNbr, columnNbr):
@@ -31,14 +35,19 @@ class Board:
         self._fields = {(row, column): Field(row, column)
                         for row in range(self._rowNumber)
                         for column in range(self._columnNumber)}
-            
+
     def _removeField(self, row, column):
         if (row, column) in self._fields:
             del(self._fields[row, column])
 
     def _getAvailableStartFields(self, shipSize):
-        start = self._getFreeFields(shipSize, Direction.HORIZONTAL, lambda i,j: (i, j), (self._rowNumber, self._columnNumber))
-        start.extend(self._getFreeFields(shipSize, Direction.VERTICAL, lambda i,j: (j, i), (self._columnNumber, self._rowNumber)))
+        start = self._getFreeFields(
+            shipSize, Direction.HORIZONTAL, lambda i, j: (
+                i, j), (self._rowNumber, self._columnNumber))
+        start.extend(
+            self._getFreeFields(
+                shipSize, Direction.VERTICAL, lambda i, j: (
+                    j, i), (self._columnNumber, self._rowNumber)))
         return start
 
     def _getFreeFields(self, size, direction, key, borders):
@@ -50,7 +59,7 @@ class Board:
                     freeFields += 1
                     if freeFields >= size:
                         end = j + 1 - size
-                        pair = self._fields[key(i, end)], direction  
+                        pair = self._fields[key(i, end)], direction
                         start.append(pair)
                 else:
                     freeFields = 0
@@ -59,7 +68,7 @@ class Board:
     def extractShipFields(self, startField, direction, size):
         fields = self._shipFields(startField, direction, size)
         self._removeAdjacentFields(fields)
-      
+
         return fields
 
     def _shipFields(self, startField, direction, size):
@@ -90,7 +99,7 @@ class Board:
     def __str__(self):
         visual = "   "
         for column in range(self._columnNumber):
-            visual = visual + chr(column  + ord('A'))
+            visual = visual + chr(column + ord('A'))
 
         for row in range(self._rowNumber):
             visual = "{}\n{:2}|".format(visual, row + 1)
@@ -103,32 +112,34 @@ class Board:
             visual = visual + '|'
         return visual
 
+
 class Ship:
     def __init__(self, fields):
         self._fields = fields
         self._hitFields = set()
 
     def shootField(self, row, column):
-       
+
         for field in self._fields:
             print(self._hitFields)
             if field._row == row and field._column == column:
                 self._hitFields.add(field)
-            
-                return (ShootingResults.SHIP_DESTROYED if self.isItDestroyed() 
-                                                       else ShootingResults.HIT)
+
+                return (ShootingResults.SHIP_DESTROYED if self.isItDestroyed()
+                        else ShootingResults.HIT)
 
         return ShootingResults.MISS
 
     def isItDestroyed(self):
-        return len(self._hitFields) == len(self._fields) 
+        return len(self._hitFields) == len(self._fields)
+
 
 class Shipbuilder:
-    
+
     def getFleet(self, rowsNumber, columnsNumber, shipSizes):
         fleet = None
-        
-        while fleet == None:         
+
+        while fleet is None:
             board = Board(rowsNumber, columnsNumber)
             fleet = self._makeFleet(board, shipSizes)
         return fleet
@@ -137,7 +148,7 @@ class Shipbuilder:
         fleet = Fleet()
         for size in shipSizes:
             fields = self._putShip(board, size)
-       
+
             if not fields:
                 return None
 
@@ -152,11 +163,12 @@ class Shipbuilder:
         choosenField = self._getStartingField(startingFields)
         field = choosenField[0]
         direction = choosenField[1]
-       
+
         return board.extractShipFields(field, direction, size)
 
     def _getStartingField(self, availableFields):
         return random.choice(availableFields)
+
 
 class Fleet:
 
@@ -173,8 +185,8 @@ class Fleet:
             if shootingResult == ShootingResults.HIT:
                 return ShootingResults.HIT
             if shootingResult == ShootingResults.SHIP_DESTROYED:
-                return (ShootingResults.FLEET_DESTROYED if self.allShipsDestroyed() 
-                                                        else ShootingResults.SHIP_DESTROYED)
+                return (ShootingResults.FLEET_DESTROYED if self.allShipsDestroyed(
+                ) else ShootingResults.SHIP_DESTROYED)
         return ShootingResults.MISS
 
     def allShipsDestroyed(self):
@@ -200,11 +212,13 @@ class Fleet:
 
 # Game logic
 
+
 class ShootingResults:
     MISS = 0
     HIT = 1
     SHIP_DESTROYED = 2
     FLEET_DESTROYED = 3
+
 
 class FieldState:
     AVAILABLE = 0
@@ -213,10 +227,12 @@ class FieldState:
     HIT = 3
     SHIP_DESTROYED = 4
 
+
 class ShootingFase:
     RANDOM = 0
     AFTER_FIRST_HIT = 1
     DESROY_SHIP = 2
+
 
 class HitField:
     def __init__(self, row, column):
@@ -230,13 +246,15 @@ class HitField:
     def isItAvailable(self):
         return self._fieldState == FieldState.AVAILABLE
 
+
 class HitBoard:
 
     def __init__(self, rowsNbr, columnsNbr):
         self._rowsNumber = rowsNbr
         self._columnsNumber = columnsNbr
 
-        self._fields = {(r, c):HitField(r, c) for r in range(self._rowsNumber) for c in range(self._columnsNumber)}
+        self._fields = {(r, c): HitField(r, c) for r in range(
+            self._rowsNumber) for c in range(self._columnsNumber)}
 
     def getShipFields(self, shipSize):
         fields = self._fieldsForHorizontalShip(shipSize)
@@ -244,10 +262,20 @@ class HitBoard:
         return fields
 
     def _fieldsForHorizontalShip(self, shipSize):
-        return self._shipFields(shipSize, lambda i, j:  self._fields[i, j], self._rowsNumber, self._columnsNumber)
+        return self._shipFields(shipSize,
+                                lambda i,
+                                j: self._fields[i,
+                                                j],
+                                self._rowsNumber,
+                                self._columnsNumber)
 
     def _fieldsForVerticalShIps(self, shipSize):
-        return self._shipFields(shipSize, lambda i, j:  self._fields[j, i], self._columnsNumber, self._rowsNumber)
+        return self._shipFields(shipSize,
+                                lambda i,
+                                j: self._fields[j,
+                                                i],
+                                self._columnsNumber,
+                                self._rowsNumber)
 
     def _shipFields(self, size, getField, imax, jmax):
         fields = []
@@ -263,7 +291,7 @@ class HitBoard:
                 else:
                     places = 0
         return fields
-    
+
     def recordShot(self, field, shootingResult):
         if shootingResult == ShootingResults.MISS:
             field.recordResult(FieldState.MISS)
@@ -304,18 +332,18 @@ class HitBoard:
         if fields[0]._row == fields[-1]._row:
             row = fields[0]._row
             self._removeField((row, fields[0]._column - 1))
-            self._removeField((row, fields[-1]._column + 1,))      
+            self._removeField((row, fields[-1]._column + 1,))
 
     def fieldsSameDirection(self, field, direction):
         row = field._row
         column = field._column
 
         if direction == FieldDirection.LEFT:
-            span = range(column - 1, -1,  -1) 
+            span = range(column - 1, -1, -1)
         elif direction == FieldDirection.RIGHT:
             span = range(column + 1, self._columnsNumber)
         elif direction == FieldDirection.UP:
-            span = range(row - 1, -1,  -1)
+            span = range(row - 1, -1, -1)
         else:
             span = range(row + 1, self._rowsNumber)
 
@@ -332,9 +360,10 @@ class HitBoard:
                 if self._field[r, column].isItAvailable():
                     fields.append(self._fields[r, column])
                 else:
-                    break     
+                    break
 
         return fields
+
 
 class Artillery:
     def __init__(self, rowsNbr, colsNbr, shipSizes):
@@ -344,9 +373,9 @@ class Artillery:
         self._f2 = ShootingFase.AFTER_FIRST_HIT
         self._f3 = ShootingFase.DESROY_SHIP
         self._tacticts = {
-                           self._f1: RandomChooseField,
-                           self._f2: ChooseFieldAfterFirstHit,
-                           self._f3: ChooseFieldSystematicShooting}
+            self._f1: RandomChooseField,
+            self._f2: ChooseFieldAfterFirstHit,
+            self._f3: ChooseFieldSystematicShooting}
         self._startRandomShooting()
         self._shootField = None
         self._fieldsShootShip = []
@@ -356,13 +385,14 @@ class Artillery:
         return self._shootField
 
     def processResults(self, shootingResults):
-    
+
         if shootingResults == ShootingResults.MISS:
             self._grid.recordShot(self._shootField, shootingResults)
             return False
 
         self._fieldsShootShip.append(self._shootField)
-        self._fieldsShootShip.sort(key=lambda field: field._row + field._column)
+        self._fieldsShootShip.sort(
+            key=lambda field: field._row + field._column)
 
         self._grid._removeField(self._shootField)
 
@@ -379,18 +409,18 @@ class Artillery:
 
     def _startRandomShooting(self):
         self._shootingFase = ShootingFase.RANDOM
-        self._shootingTactict = self._tacticts[self._shootingFase] \
-            (self._grid, self._getLongestShipForShooting())
+        self._shootingTactict = self._tacticts[self._shootingFase](
+            self._grid, self._getLongestShipForShooting())
 
     def _startShootingFieldAfterFirstHit(self):
         self._shootingFase = ShootingFase.AFTER_FIRST_HIT
-        self._shootingTactict = self._tacticts[self._shootingFase] \
-            (self._grid, self._shootField)
+        self._shootingTactict = self._tacticts[self._shootingFase](
+            self._grid, self._shootField)
 
     def _startSystematicShooting(self):
         self._shootingFase = ShootingFase.SHIP_DESTROYED
-        self._shootingTactict = self._tacticts[self._shootingFase] \
-            (self._grid, self._fieldsShootShip)
+        self._shootingTactict = self._tacticts[self._shootingFase](
+            self._grid, self._fieldsShootShip)
 
     def _getLongestShipForShooting(self):
         return max(self._shipSizes)
@@ -398,8 +428,9 @@ class Artillery:
     def addNewTactic(self, shootingFase, shootingTactict):
         self._tacticts[shootingFase] = shootingTactict
 
+
 class RandomChooseField:
-    
+
     def __init__(self, grid, shipSize):
         self._grid = grid
         self._shipSize = shipSize
@@ -408,11 +439,13 @@ class RandomChooseField:
         fields = self._grid.getShipFields(self._shipSize)
         return random.choice(fields)
 
+
 class FieldDirection:
     LEFT = 0
     UP = 1
     RIGHT = 2
     DOWN = 3
+
 
 class ChooseFieldAfterFirstHit:
     def __init__(self, grid, hitField):
@@ -420,17 +453,20 @@ class ChooseFieldAfterFirstHit:
         self._hitField = hitField
 
     def chooseField(self):
-        nearbyFields = [self._grid.fieldsSameDirection(self._hitField, FieldDirection.LEFT),
-                        self._grid.fieldsSameDirection(self._hitField, FieldDirection.UP),
-                        self._grid.fieldsSameDirection(self._hitField, FieldDirection.RIGHT),
-                        self._grid.fieldsSameDirection(self._hitField, FieldDirection.DOWN)]
+        nearbyFields = [
+            self._grid.fieldsSameDirection(
+                self._hitField, FieldDirection.LEFT), self._grid.fieldsSameDirection(
+                self._hitField, FieldDirection.UP), self._grid.fieldsSameDirection(
+                self._hitField, FieldDirection.RIGHT), self._grid.fieldsSameDirection(
+                    self._hitField, FieldDirection.DOWN)]
 
         longestDirection = max(nearbyFields, key=lambda i: len(i))
 
         return longestDirection[0]
 
+
 class ChooseFieldSystematicShooting:
-    
+
     def __init__(self, grid, shipsHit):
         self._grid = grid
         self._fieldsShotShip = shipsHit
@@ -440,15 +476,23 @@ class ChooseFieldSystematicShooting:
         lastField = self._fieldsShotShip[-1]
 
         if firstField._row == lastField._row:
-            nearby = [self._grid.fieldsSameDirection(firstField, FieldDirection.LEFT),
-                      self._grid.fieldsSameDirection(lastField, FieldDirection.RIGHT)]
+            nearby = [
+                self._grid.fieldsSameDirection(
+                    firstField,
+                    FieldDirection.LEFT),
+                self._grid.fieldsSameDirection(
+                    lastField,
+                    FieldDirection.RIGHT)]
         else:
-            nearby = [self._grid.fieldsSameDirection(firstField, FieldDirection.UP),
-                      self._grid.fieldsSameDirection(firstField, FieldDirection.DOWN)]
+            nearby = [
+                self._grid.fieldsSameDirection(
+                    firstField, FieldDirection.UP), self._grid.fieldsSameDirection(
+                    firstField, FieldDirection.DOWN)]
 
         longestDirection = max(nearby, key=lambda i: len(i))
 
         return longestDirection[0]
+
 
 def EnterScore():
     while True:
@@ -462,8 +506,8 @@ def EnterScore():
             return ShootingResults.SHIP_DESTROYED
         print("Invalid input!")
 
-ships = [5, 4, 4, 3, 3, 3, 2, 2, 2, 2]
 
+ships = [5, 4, 4, 3, 3, 3, 2, 2, 2, 2]
 
 
 print("Enter the field that you want to hit (row-column)")
